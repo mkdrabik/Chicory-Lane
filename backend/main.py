486d5fe42536314588acfe39201fa4f2d41a24a6
Mason Drabik <mkdrabik@unc.edu>
2025-10-08 00:33:42 -0400

    be folder

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-from api_calls import search_with_context   
+from api_calls import search_with_context
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
