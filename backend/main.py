336a57039ccea60dc29477f73bd49211fedb7814
Mason Drabik <mkdrabik@unc.edu>
2025-11-17 12:50:03 -0500

    cors

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,9 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://chicorylane.netlify.app"
+    "https://chicorylane.netlify.app",
+    "https://www.chicorylane.com"
+
 ]
 
 app.add_middleware(
