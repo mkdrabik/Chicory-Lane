33dc853b491dfb4db1990e7a2810857861c44896
Mason Drabik <mkdrabik@unc.edu>
2025-11-20 12:36:56 -0500

    work

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,8 @@ origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://chicorylane.netlify.app",
-    "https://chicorylane.com"
+    "https://chicorylane.com",
+    "https://www.chicorylane.com"
 ]
 
 app.add_middleware(
